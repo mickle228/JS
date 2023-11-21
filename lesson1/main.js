2d0bf03cedb533ceef19c643505d6798942a7f74
mickle228 <olehkachan05@gmail.com>
2023-11-21 19:09:40 +0200

    lesson1

---
+++ lesson1/main.js
@@ -0,0 +1,37 @@
+let hello = 'hello';
+let owu = 'owu';
+let com = 'com'
+let ua = 'ua'
+let number1 = 1;
+let number10= 10;
+let number999 = -999
+let number123= 123
+let number3_14= 3.14
+let number2_7= 2.7
+let number16= 16
+let atrue= true
+let afalse= false
+
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(number1);
+console.log(number10);
+console.log(number999);
+console.log(number123);
+console.log(number3_14);
+console.log(number2_7);
+console.log(number16);
+console.log(atrue);
+console.log(afalse);
+
+let firstName = 'Oleh'
+let middleName = 'Artur'
+let lastName = 'Kachanovskyi'
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+let a = 100; let b = '100'; let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c)
