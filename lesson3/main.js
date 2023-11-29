46ae09d81541b9c9defedf5e32900e2d45006675
mickle228 <olehkachan05@gmail.com>
2023-11-29 23:49:43 +0200

    lesson3

--- lesson3/main.js
+++ lesson3/main.js
@@ -5,6 +5,7 @@ for (let i = 0; i < 10; i++) {
     document.write(`</div>`)
 }
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+debugger;
 for (let i = 0; i < 10; i++) {
     document.write(`<div>`)
     document.write(`div number ${i}`)
